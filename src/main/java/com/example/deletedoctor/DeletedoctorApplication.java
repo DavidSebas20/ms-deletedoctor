c392953011c0f0b7c01a11778daca77427d53114
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-08 16:39:21 -0500

    :tada: Initialize Deletedoctor project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/deletedoctor/DeletedoctorApplication.java
@@ -0,0 +1,13 @@
+package com.example.deletedoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeletedoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeletedoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deletedoctor/DeletedoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deletedoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeletedoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
